9bcc4fd0bcd4ba29c9ad1f2c7a04d1ebe11ba6c4
Darrell-Rafa <drafa0609@gmail.com>
2023-02-11 19:11:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [synonymList, setSynonymList] = useState([]);
+  const [word, setWord] = useState({
+    onChangeWord: "",
+    finalWord: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  function getSynonym(word) {
+    setIsLoading(true);
+
+    fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setSynonymList(res.map((data) => data.word));
+        setIsLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    getSynonym(word.finalWord);
+  }, [word.finalWord]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Get Synonyms</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setWord((prevWord) => {
+            return {
+              ...prevWord,
+              finalWord: word.onChangeWord,
+            };
+          });
+        }}
+      >
+        <label htmlFor="word-input">Your Word: </label>
+        <input
+          type="text"
+          id="word-input"
+          onChange={(e) =>
+            setWord((prevWord) => {
+              return {
+                ...prevWord,
+                onChangeWord: e.target.value,
+              };
+            })
+          }
+          value={word.onChangeWord}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      {isLoading ? (
+        <p className="loading-text">Loading...</p>
+      ) : (
+        <ul>
+          {synonymList.length !== 0 ? (
+            <>
+              <p className="found-text">Synonym(s) of {word.finalWord}:</p>
+              {synonymList.map((synonym, index) => (
+                <li
+                  key={index}
+                  onClick={() =>
+                    setWord({
+                      onChangeWord: synonym,
+                      finalWord: synonym,
+                    })
+                  }
+                >
+                  {synonym}
+                </li>
+              ))}
+            </>
+          ) : (
+            word.finalWord !== "" && (
+              <p className="not-found-text">
+                No synonyms found for "{word.finalWord}"
+              </p>
+            )
+          )}
+        </ul>
+      )}
     </div>
   );
 }
