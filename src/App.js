36a9a7b5f879dc7b765b139c80a0ac12a1d21675
Darrell-Rafa <drafa0609@gmail.com>
2023-02-12 01:04:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,88 +1,66 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-function App() {
-  const [synonymList, setSynonymList] = useState([]);
-  const [word, setWord] = useState({
-    onChangeWord: "",
-    finalWord: "",
+function CheckboxRow({ label }) {
+  const [permissions, setPermissions] = useState({
+    read: false,
+    write: false,
+    delete: false,
   });
-  const [isLoading, setIsLoading] = useState(false);
 
-  function getSynonym(word) {
-    setIsLoading(true);
+  console.log(permissions);
 
-    fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setSynonymList(res.map((data) => data.word));
-        setIsLoading(false);
+  function handleCheckboxChange(type, isChecked) {
+    if (type === "delete") {
+      setPermissions((prev) => {
+        return isChecked
+          ? { ...prev, read: true, write: true, delete: true }
+          : { ...prev, delete: false };
       });
+    } else {
+      setPermissions((prev) => {
+        return isChecked
+          ? { ...prev, [type]: true }
+          : { ...prev, [type]: false, delete: false };
+      });
+    }
   }
 
-  useEffect(() => {
-    getSynonym(word.finalWord);
-  }, [word.finalWord]);
+  return (
+    <>
+      <label>{label}</label>
+      <input
+        type="checkbox"
+        checked={permissions.read}
+        onChange={(e) => handleCheckboxChange("read", e.target.checked)}
+      />
+      <input
+        type="checkbox"
+        checked={permissions.write}
+        onChange={(e) => handleCheckboxChange("write", e.target.checked)}
+      />
+      <input
+        type="checkbox"
+        checked={permissions.delete}
+        onChange={(e) => handleCheckboxChange("delete", e.target.checked)}
+      />
+    </>
+  );
+}
 
+function App() {
   return (
     <div className="app-container">
-      <h1>Get Synonyms</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setWord((prevWord) => {
-            return {
-              ...prevWord,
-              finalWord: word.onChangeWord,
-            };
-          });
-        }}
-      >
-        <label htmlFor="word-input">Your Word: </label>
-        <input
-          type="text"
-          id="word-input"
-          onChange={(e) =>
-            setWord((prevWord) => {
-              return {
-                ...prevWord,
-                onChangeWord: e.target.value,
-              };
-            })
-          }
-          value={word.onChangeWord}
-        />
-        <input type="submit" value="Submit" />
-      </form>
-      {isLoading ? (
-        <p className="loading-text">Loading...</p>
-      ) : (
-        <ul>
-          {synonymList.length !== 0 ? (
-            <>
-              <p className="found-text">Synonym(s) of {word.finalWord}:</p>
-              {synonymList.map((synonym, index) => (
-                <li
-                  key={index}
-                  onClick={() =>
-                    setWord({
-                      onChangeWord: synonym,
-                      finalWord: synonym,
-                    })
-                  }
-                >
-                  {synonym}
-                </li>
-              ))}
-            </>
-          ) : (
-            word.finalWord !== "" && (
-              <p className="not-found-text">
-                No synonyms found for "{word.finalWord}"
-              </p>
-            )
-          )}
-        </ul>
-      )}
+      <div className="grid">
+        <span></span>
+        <label>read</label>
+        <label>write</label>
+        <label>delete</label>
+        <CheckboxRow label="t1" />
+        <CheckboxRow label="t2" />
+        <CheckboxRow label="t3" />
+        <CheckboxRow label="t4" />
+      </div>
+      {/* <button>Submit</button> */}
     </div>
   );
 }
